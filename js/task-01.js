facc283a89d137417f54a976bec134c0d27dad38
KoreshkovS <110051047+KoreshkovS@users.noreply.github.com>
2023-03-31 22:17:32 +0300

    testing code

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,41 @@
-const subscription = "pro";
+// let balance = 10000;
+// const payment = 2000;
 
-const canAccessContent = subscription === "pro" || subscription === "vip";
+// console.log(
+//   `загальна вартість заказу ${payment} кредитів. Перевіряєм доступність коштів на вашому рахунку`
+// );
+// if (balance >= payment) {
+//   balance = balance - payment;
+//   console.log(`на рахунку лишилось ${balance} кредитів`);
+// } else {
+//   console.log(`на рахунку не достатньо кредитів`);
+// }
+// console.log("операція завершена");
 
-console.log("Є доступ к контенту?", canAccessContent);
+const totalSpent = 2000;
+let payment2 = 500;
+let discount = 0;
+let discountPercent;
+
+if (totalSpent <= 100) {
+  console.log("у вас немає партнерської скидки");
+  discountPercent = "0%";
+} else if (totalSpent >= 100 && totalSpent <= 1000) {
+  console.log("ви бронзовий партнер зі знижкою 2%");
+  discount = 0.02;
+  discountPercent = "2%";
+} else if (totalSpent > 1000 && totalSpent < 5000) {
+  console.log("ви срібний партнер зі знижкою 5%");
+  discount = 0.05;
+  discountPercent = "5%";
+} else if (totalSpent >= 5000) {
+  console.log("ви золотий партнер зі знижкою 10%");
+  discount = 0.1;
+  discountPercent = "10%";
+}
+
+let totalPeyment = payment2 - payment2 * discount;
+
+console.log(
+  `Оформлюєм замовлення на сумму ${totalPeyment} з урахуванням знижки ${discountPercent}`
+);
