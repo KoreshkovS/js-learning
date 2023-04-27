f95f09b41e8f87dfbb7b5a33af9b93f6be2d6160
KoreshkovS <110051047+KoreshkovS@users.noreply.github.com>
2023-04-27 14:26:30 +0300

    testing

---
+++ js/task-01.js
@@ -0,0 +1,24 @@
+const subs = [
+  "user1",
+  "user2",
+  "user3",
+  "user4",
+  "user5",
+  "user6",
+  "user7",
+  "user8",
+  "user9",
+  "user10",
+  "user11",
+  "user12",
+  "user13",
+  "user14",
+  "user15",
+  "user16",
+];
+
+console.log(subs.length);
+
+if (subs.length > 15) {
+  alert(" увас багато підписників");
+}
