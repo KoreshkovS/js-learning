99e0db2632203adb2a5e398c8e5c8184e65e0149
KoreshkovS <110051047+KoreshkovS@users.noreply.github.com>
2023-05-01 15:27:24 +0300

    playing with code

--- js/task-01.js
+++ js/task-01.js
@@ -1,24 +1,15 @@
-const subs = [
-  "user1",
-  "user2",
-  "user3",
-  "user4",
-  "user5",
-  "user6",
-  "user7",
-  "user8",
-  "user9",
-  "user10",
-  "user11",
-  "user12",
-  "user13",
-  "user14",
-  "user15",
-  "user16",
-];
+const fnA = function () {
+  console.log(" function A");
+};
+const fnB = function () {
+  console.log(" function B");
+};
+const fnC = function () {
+  console.log(" function C");
+};
 
-console.log(subs.length);
+fnA();
 
-if (subs.length > 15) {
-  alert(" увас багато підписників");
-}
+fnB();
+
+fnC();
